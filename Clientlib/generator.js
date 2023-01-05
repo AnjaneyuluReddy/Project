87f6c68b8d324c1fe4b45372fb7e6eaa5b3b89ba
Anjaneyulu <anjaneyulureddy1999@gmail.com>
2023-01-05 14:51:51 -0500

    pin change

--- Clientlib/generator.js
+++ Clientlib/generator.js
@@ -2,6 +2,6 @@ const generateButton = document.getElementById('generate-button');
 const pinDisplay = document.getElementById('pin-display');
 
 generateButton.addEventListener('click', () => {
-  const pin = Math.floor(Math.random() * 10000).toString().padStart(4, '0');
+  const pin = Math.floor(Math.random() * 1000000).toString().padStart(6, '0');
   pinDisplay.textContent = pin;
 });
